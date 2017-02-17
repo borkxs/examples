95a8c2b29e20202e7de38ec5f7c39853eb25b437
Erik Michaelson <erik.michaelson@gmail.com>
2017-02-17 16:32:21 -0500

    Add create-react-app directory, new hash-base-image thing

---
+++ create-react-app/hash-base-image/README.md
@@ -0,0 +1,7 @@
+# hash-base-image
+
+This is a dumb little client app to help testing a wacky service idea.
+
+Open:
+
+https://borkxs.github.io/examples/create-react-app/hash-base-image/build
---
+++ create-react-app/hash-base-image/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const INITIAL = `{
+  "dataSets": [{
+    "data": [{"x": 10, "y": 10}, {"x": 20, "y": 30}, {"x": 40, "y": 10}]
+  }]
+}`
+
+class App extends Component {
+  state = { json: INITIAL }
+
+  render() {
+    try {
+      var hash = btoa(JSON.stringify(JSON.parse(this.state.json)))
+    } catch(e) {
+      hash = "ERROR: BAD ENTRY"
+    }
+    const url = `http://react-component-as-a-service.herokuapp.com/chart/${hash}.png`
+
+    return (
+      <div className="App">
+        <p>json:</p>
+        <p><textarea className="App-textarea" type="text" value={this.state.json}
+          onChange={evt => this.setState({ json: evt.target.value })}
+          /></p>
+        <p>hash: {hash}</p>
+        <p>url: {url}</p>
+        <p><img src={url}  alt="" width="600" height="400" /></p>
+      </div>
+    )
+  }
+}
+
+export default App;
